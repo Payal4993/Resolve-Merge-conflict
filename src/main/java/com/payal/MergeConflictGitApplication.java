ae887d1e259474d5ff8b1ea4dcb999c8bcc56aae
Payal4993 <prepayal4@gmail.com>
2022-05-05 09:51:52 +0530

    Sts commit

---
+++ src/main/java/com/payal/MergeConflictGitApplication.java
@@ -0,0 +1,13 @@
+package com.payal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergeConflictGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MergeConflictGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payal/MergeConflictGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergeConflictGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
